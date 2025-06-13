ac6b2dc27c51cd16ca681f08c83184bdd04d9609
Valerii Hud <support@valerii-hud.com>
2025-06-13 06:03:11 +0200

    Added homework 13.06.2025

--- 12.06.2025/01_Homework/script.js
+++ 12.06.2025/01_Homework/script.js
@@ -1,15 +1,28 @@
-const btn = document.querySelector('button'),
-  counter = document.querySelector('h1 > span');
+const btns = document.querySelectorAll('button'),
+  counter = document.querySelector('h1 > span'),
+  overlay = document.querySelector('.overlay');
 let i = 1;
 
 const nextNumberOnCounter = (e) => {
+  console.log(e.currentTarget);
+  console.log(e.type);
   counter.textContent = `${i + 1}`;
   i++;
-  if (i == 10) {
-    btn.removeEventListener('click', nextNumberOnCounter);
-    btn.textContent = 'Finish';
-  }
+  // if (i == 10) {
+  //   btn.removeEventListener('click', nextNumberOnCounter);
+  //   btn.textContent = 'Finish';
+  // }
 };
 
-btn.textContent = 'Click me and see on counter';
-btn.addEventListener('click', nextNumberOnCounter);
+btns.forEach((btn) => {
+  btn.textContent = 'Click me and see on counter';
+  btn.addEventListener('click', nextNumberOnCounter);
+});
+
+overlay.addEventListener('click', nextNumberOnCounter);
+
+const link = document.querySelector('a');
+link.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log(e.target);
+});
