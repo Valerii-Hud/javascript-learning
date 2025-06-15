1b0b8585ad30f7090e12f00e870198c2b636df2a
Valerii Hud <support@valerii-hud.com>
2025-06-15 10:14:30 +0200

    Added homework 15.06.2025 (10:10)

---
+++ 15.06.2025/02_Homework/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const box = document.querySelector('.box'),
+  block = document.querySelector('.block');
+
+console.log(block);
+
+console.log(block?.textContent);
+
+console.log(1 + 2);
