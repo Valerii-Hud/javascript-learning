9124a34c35da58ba7d90c2adf4e7224fd86b4c00
Valerii Hud <support@valerii-hud.com>
2025-06-24 10:21:27 +0200

    added homework 24.06.2025 (10:20)

---
+++ 24.06.2025/02_Homework/script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const user = {
+  name: 'Alex',
+  surname: 'Smith',
+  birthday: '20/04/1993',
+  showMyPublicData: () => {
+    console.log(`${this.name} ${this.surname}`);
+  },
+};
+
+console.log(Object.getOwnPropertyDescriptors(user));
+
+Object.defineProperty(user, 'name', {
+  writable: false,
+});
+user.name = 'qfwknfqw';
+// writable
+// enumerable
+// configurable
