d8a7a6a55d7c83089ca5614490a275614f3acfbc
Valerii Hud <support@valerii-hud.com>
2025-06-27 18:21:54 +0200

    Added homework 27.06.2025 (18:20)

--- 26.06.2025/04_Homework/script.js
+++ 26.06.2025/04_Homework/script.js
@@ -1,9 +1,33 @@
 const btn = document.querySelector('.btn');
 
-let timerId;
-btn.addEventListener('click', () => {
-  timerId = setTimeout(logger, 2000, 'Hello');
-});
-function logger(textToSay) {
-  console.log(textToSay);
+let i = 0;
+
+function translateAnimation() {
+  const elem = document.querySelector('.box');
+  let pos = 0;
+
+  // const timerId = setInterval(frame, 10);
+
+  let timerId = setTimeout(function frameRequest() {
+    frame();
+    timerId = setTimeout(frameRequest, 10);
+  }, 10);
+  function frame() {
+    if (pos == 300) clearInterval(timerId);
+    else {
+      pos++;
+      elem.style.top = `${pos}px`;
+      elem.style.left = `${pos}px`;
+    }
+  }
 }
+
+btn.addEventListener('click', translateAnimation);
+// function logger(textToSay) {
+//   console.log(textToSay);
+// }
+
+// let id = setTimeout(function log() {
+//   console.log('Hello');
+//   id = setTimeout(log, 500);
+// }, 500);
