3912b751715272463a436f088d0338853f4433fc
Valerii Hud <support@valerii-hud.com>
2025-07-04 20:34:31 +0200

    Added homework 04.07.2025 (20:30)

---
+++ 04.07.2025/01_Homework/script.js
@@ -0,0 +1,11 @@
+// const log = function (a, b, ...rest) {
+//   console.log(a, b, rest);
+// };
+
+// log('basic', 'rest', 'operator', 'usage');
+
+function calcOrDouble(num, basis = 2) {
+  console.log(num * basis);
+}
+
+calcOrDouble(3);
