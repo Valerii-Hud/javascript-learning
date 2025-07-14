a06446f92e8e27481b081567812e3726d9f4d743
Valerii Hud <support@valerii-hud.com>
2025-07-14 15:15:43 +0200

    added homework 14.07.2025 (15:00)

---
+++ 14.07.2025/01_Homework/script.js
@@ -0,0 +1,22 @@
+// const string = 'Something here';
+// const reg = /h/;
+// console.log(string.search(reg));
+
+// const pass = 'zaq1.@.WSX';
+// console.log(pass.replace(/./g, '*'));
+// console.log(pass.replace(/\./g, '*'));
+
+// const code = 'XDW-GDW-LCS-DOE';
+// console.log(code.replace(/-/g, '_'));
+
+// const user = 'root';
+// console.log(/root/.test(user));
+
+// const price = '832USDT, 310USDT, 122USDT';
+// const reg = /\d\d\d/g;
+// console.log(price.match(reg));
+
+// const name = 'R2D2';
+// const str = `My name is ${name}`;
+
+// console.log(str.match(/\w\d\w\d/gi));
