def13252d476211545f2fefca15a713a6a702866
Valerii Hud <support@valerii-hud.com>
2025-07-25 14:06:05 +0200

    Added homework 25.07.2025 (14:00)

---
+++ 25.07.2025/03_Homework/script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// Example No 1
+// console.log(1);
+
+// setTimeout(() => {
+//   console.log('Timeout');
+// }, 2000);
+
+// setTimeout(() => {
+//   console.log('Timeout');
+// }, 4000);
+
+// console.log(2);
+
+setTimeout(() => {
+  console.log(1);
+}, 0);
+
+console.log(2);
